7bffab86b5b6d28ecafc48044d3920ec1c22e554
Zalbani <pro.pierson.alban@gmail.com>
2020-01-30 12:52:53 +0100

    Add annimations

--- pages/index.vue
+++ pages/index.vue
@@ -1,88 +1,82 @@
 <template>
   <section>
     <div class="options">
-      <div class="option active" style="--optionBackground:url(https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg);">
+      <div
+        v-for="category in categories"
+        :key="category.id"
+        v-on:click="activate(category.id)"
+        v-bind:class="{ active : activeCat == category.id }"
+        v-bind:style="{ backgroundImage: 'url(' + category.bgImage + ')' }"
+        class="option"
+      >
         <div class="shadow" />
         <div class="label">
           <div class="icon">
-            <i class="fas fa-walking" />
+            <i
+              v-bind:class="category.fontAwsomeIcon"
+              class="fas"
+            />
           </div>
           <div class="infoCategory">
             <div class="main">
-              Blonkisoaz
+              {{ category.mainInfo }}
             </div>
             <div class="sub">
-              Omuke trughte a otufta
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="option" style="--optionBackground:url(https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg);">
-        <div class="shadow" />
-        <div class="label">
-          <div class="icon">
-            <i class="fas fa-snowflake" />
-          </div>
-          <div class="infoCategory">
-            <div class="mainCategory">
-              Oretemauw
-            </div>
-            <div class="sub">
-              Omuke trughte a otufta
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="option" style="--optionBackground:url(https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg);">
-        <div class="shadow" />
-        <div class="label">
-          <div class="icon">
-            <i class="fas fa-tree" />
-          </div>
-          <div class="infoCategory">
-            <div class="mainCategory">
-              Iteresuselle
-            </div>
-            <div class="sub">
-              Omuke trughte a otufta
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="option" style="--optionBackground:url(https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg);">
-        <div class="shadow" />
-        <div class="label">
-          <div class="icon">
-            <i class="fas fa-tint" />
-          </div>
-          <div class="infoCategory">
-            <div class="mainCategory">
-              Idiefe
-            </div>
-            <div class="sub">
-              Omuke trughte a otufta
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="option" style="--optionBackground:url(https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg);">
-        <div class="shadow" />
-        <div class="label">
-          <div class="icon">
-            <i class="fas fa-sun" />
-          </div>
-          <div class="infoCategory">
-            <div class="mainCategory">
-              Inatethi
-            </div>
-            <div class="sub">
-              Omuke trughte a otufta
+              {{ category.secondaryinfo }}
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    <a href="http://victorofvalencia-blog.tumblr.com" target="_blank" class="credit">Photos from Victor of Valencia on tumblr</a>
   </section>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        {
+          id: 1,
+          fontAwsomeIcon: 'fa-walking',
+          bgImage: 'https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg',
+          mainInfo: 'Blonkisoaz',
+          secondaryinfo: 'Omuke trughte a otufta'
+        },
+        {
+          id: 2,
+          fontAwsomeIcon: 'fa-snowflake',
+          bgImage: 'https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg',
+          mainInfo: 'Blonkisoaz',
+          secondaryinfo: 'Omuke trughte a otufta'
+        },
+        {
+          id: 3,
+          fontAwsomeIcon: 'fa-tree',
+          bgImage: 'https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg',
+          mainInfo: 'Blonkisoaz',
+          secondaryinfo: 'Omuke trughte a otufta'
+        },
+        {
+          id: 4,
+          fontAwsomeIcon: 'fa-tint',
+          bgImage: 'https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg',
+          mainInfo: 'Blonkisoaz',
+          secondaryinfo: 'Omuke trughte a otufta'
+        }, {
+          id: 5,
+          fontAwsomeIcon: 'fa-sun',
+          bgImage: 'https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg',
+          mainInfo: 'Blonkisoaz',
+          secondaryinfo: 'Omuke trughte a otufta'
+        }
+      ],
+      activeCat: 1
+    }
+  },
+  methods: {
+    activate (cat) {
+      this.activeCat = cat
+    }
+  }
+}
+</script>
