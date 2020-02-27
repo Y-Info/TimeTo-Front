3e19329a9c6419c914b920d5b5120d41ce1db1f8
Restyled.io <commits@restyled.io>
2020-02-27 15:23:39 +0000

    Restyled by clang-format

--- store/index.js
+++ store/index.js
@@ -1,18 +1,14 @@
-export const state = () => ({ authUser: null })
+export const state = () => ({authUser : null})
 
 export const mutations = {
-  SET_USER (state, user) {
-    state.authUser = user
-  }
+  SET_USER(state, user) { state.authUser = user}
 }
 
 export const actions = {
-  async login ({ commit }, { email, password }) {
+  async login({commit}, {email, password}) {
     try {
-      const { data } = await this.$axios.post(
-        process.env.ApiUrl + 'user/login',
-        { email, password }
-      )
+      const {data} = await this.$axios.post(process.env.ApiUrl + 'user/login',
+                                            {email, password})
       commit('SET_USER', data)
     } catch (error) {
       if (error.response && error.response.status === 401) {
